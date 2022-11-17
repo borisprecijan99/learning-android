70f8c39c36865ef0521db2c7526c941ac0c79a09
Boris Precijan <borisprecijanbp@gmail.com>
2022-11-17 22:28:09 +0100

    Constraint layout, guideline, chain, onClick in XML

--- app/src/main/java/uns/pmf/learningandroid/MainActivity.kt
+++ app/src/main/java/uns/pmf/learningandroid/MainActivity.kt
@@ -2,22 +2,20 @@ package uns.pmf.learningandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.view.View
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var tvText: TextView
-    private lateinit var btnClickMe: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         tvText = findViewById(R.id.tvText)
-        btnClickMe = findViewById(R.id.btnClick)
+    }
 
-        btnClickMe.setOnClickListener {
-            tvText.text = "Button is clicked"
-        }
+    fun onButtonClick(view: View) {
+        tvText.text = "Button is clicked"
     }
 }
