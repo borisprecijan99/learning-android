c09f347b3822d539ec457654169d4a253659dd80
Boris Precijan <borisprecijanbp@gmail.com>
2022-11-19 21:39:42 +0100

    Message with toast

--- app/src/main/java/uns/pmf/learningandroid/SecondActivity.kt
+++ app/src/main/java/uns/pmf/learningandroid/SecondActivity.kt
@@ -2,10 +2,19 @@ package uns.pmf.learningandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class SecondActivity : AppCompatActivity() {
+    private lateinit var btnToast: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        btnToast = findViewById(R.id.btnToast)
+        btnToast.setOnClickListener {
+            Toast.makeText(this, "Message!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
