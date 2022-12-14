756e3e2dbfc4d43d8a0bf77ef403f3fc24a36da3
Boris Precijan <borisprecijanbp@gmail.com>
2022-12-14 17:18:30 +0100

    Introduction to data binding

--- app/src/main/java/uns/pmf/learningandroid/SecondActivity.kt
+++ app/src/main/java/uns/pmf/learningandroid/SecondActivity.kt
@@ -3,49 +3,36 @@ package uns.pmf.learningandroid
 import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.RadioGroup
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import uns.pmf.learningandroid.databinding.ActivitySecondBinding
 import java.util.*
 
 class SecondActivity : AppCompatActivity() {
-    private lateinit var btnRegister: Button
-    private lateinit var etFirstName: EditText
-    private lateinit var etLastName: EditText
-    private lateinit var etUsername: EditText
-    private lateinit var etPassword: EditText
-    private lateinit var rgGender: RadioGroup
-    private lateinit var btnSelectDate: Button
     private lateinit var activitySecondBinding: ActivitySecondBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        activitySecondBinding = ActivitySecondBinding.inflate(layoutInflater)
-        setContentView(activitySecondBinding.root)
+        activitySecondBinding = DataBindingUtil.setContentView(this, R.layout.activity_second)
 
-        btnRegister = activitySecondBinding.btnRegister
-        etFirstName = activitySecondBinding.etFirstName
-        etLastName = activitySecondBinding.etLastName
-        etUsername = activitySecondBinding.etUsername
-        etPassword = activitySecondBinding.etPassword
-        rgGender = activitySecondBinding.rgGender
-        btnSelectDate = activitySecondBinding.btnSelectDate
+        val user: User = User("Boris", "Precijan", "risbo99", "risbo99", "16.11.1999.", true)
+        activitySecondBinding.user = user
 
-        btnRegister.setOnClickListener {
-            val gender = if (rgGender.checkedRadioButtonId == R.id.rbFemale) "female" else "male"
-            val result = "${etFirstName.text}, ${etLastName.text}, ${etUsername.text}, ${etPassword.text}, $gender"
-            Toast.makeText(this, result, Toast.LENGTH_SHORT).show()
+        activitySecondBinding.btnRegister.setOnClickListener {
+            Toast.makeText(this, user.toString(), Toast.LENGTH_SHORT).show()
         }
 
-        btnSelectDate.setOnClickListener {
+        activitySecondBinding.btnSelectDate.setOnClickListener {
             val day = Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
             val month = Calendar.getInstance().get(Calendar.MONTH)
             val year = Calendar.getInstance().get(Calendar.YEAR)
             val datePickerDialog = DatePickerDialog(this,
-                { _, y, m, d -> btnSelectDate.text = "$d.${m + 1}.$y." }, year, month, day)
+                { _, y, m, d ->
+                    val selectedDate = "$d.${m + 1}.$y."
+                    activitySecondBinding.btnSelectDate.text = selectedDate
+                    user.dateOfBirth = selectedDate
+                }, year, month, day)
             datePickerDialog.show()
         }
     }
---
+++ app/src/main/java/uns/pmf/learningandroid/User.kt
@@ -0,0 +1,10 @@
+package uns.pmf.learningandroid
+
+data class User(
+    var firstName: String,
+    var lastName: String,
+    var userName: String,
+    var password: String,
+    var dateOfBirth: String,
+    var isMale: Boolean
+)
