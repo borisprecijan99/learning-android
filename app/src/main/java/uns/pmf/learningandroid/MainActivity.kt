5a3c6e8db4fa9ab4e4baadcf36f369e1363c1a2a
Boris Precijan <borisprecijanbp@gmail.com>
2023-01-03 17:18:53 +0100

    Two Way Data Binding

--- app/src/main/java/uns/pmf/learningandroid/MainActivity.kt
+++ app/src/main/java/uns/pmf/learningandroid/MainActivity.kt
@@ -1,36 +1,18 @@
 package uns.pmf.learningandroid
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.ViewModelProvider
+import androidx.databinding.DataBindingUtil
 import uns.pmf.learningandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var activityMainBinding: ActivityMainBinding
-    private lateinit var viewModel: MainActivityViewModel
-    private lateinit var viewModelFactory: MainActivityViewModelFactory
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(activityMainBinding.root)
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        viewModelFactory = MainActivityViewModelFactory(2022)
-        viewModel = ViewModelProvider(this, viewModelFactory).get(MainActivityViewModel::class.java)
-
-        activityMainBinding.btnOpenSecondActivity.setOnClickListener {
-            val secondActivityIntent = Intent(this, SecondActivity::class.java)
-            startActivity(secondActivityIntent)
-        }
-
-        activityMainBinding.btnIncreaseCounter.setOnClickListener {
-            viewModel.increaseCounter()
-        }
-
-        viewModel.counter.observe(this) { newCounter ->
-            activityMainBinding.textViewCounter.text = newCounter.toString()
-        }
+        activityMainBinding.myName = "Boris"
     }
 }
