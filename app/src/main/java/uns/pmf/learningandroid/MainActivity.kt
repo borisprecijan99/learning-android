5624df16064dd02c2cebce28ee656df55bf458d3
Boris Precijan <borisprecijanbp@gmail.com>
2023-01-06 21:34:12 +0100

    Options menu

--- app/src/main/java/uns/pmf/learningandroid/MainActivity.kt
+++ app/src/main/java/uns/pmf/learningandroid/MainActivity.kt
@@ -1,59 +1,45 @@
 package uns.pmf.learningandroid
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
-import android.content.Context
-import android.content.Intent
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationCompat.Action
+import android.view.*
+import android.widget.Toast
 import uns.pmf.learningandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var activityMainBinding: ActivityMainBinding
-    private var notificationManager: NotificationManager? = null
-    private val channelId = "uns.pmf.learningandroid.channel1"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(activityMainBinding.root)
-
-        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        createNotificationChannel(channelId, "Channel Name", "Channel Description")
-
-        activityMainBinding.showNotificationButton.setOnClickListener {
-            showNotification()
-        }
     }
 
-    private fun showNotification() {
-        val newActivityIntent = Intent(this, NewActivity::class.java)
-        val pendingIntent: PendingIntent =
-            PendingIntent.getActivity(this, 0, newActivityIntent, PendingIntent.FLAG_IMMUTABLE)
-        val action: Action = Action.Builder(0, "Action", pendingIntent).build()
-        val notification = NotificationCompat.Builder(this, channelId)
-            .setContentTitle("Title")
-            .setContentText("Text")
-            .setSmallIcon(R.drawable.ic_launcher_foreground)
-            .setAutoCancel(true)
-            .setPriority(NotificationCompat.PRIORITY_HIGH)
-            //.setContentIntent(pendingIntent)
-            .addAction(action)
-            .build()
-        notificationManager?.notify(2023, notification)
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
     }
 
-    private fun createNotificationChannel(id: String, name: String, description: String) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val importance = NotificationManager.IMPORTANCE_HIGH
-            val notificationChannel = NotificationChannel(id, name, importance)
-            notificationChannel.description = description
-            notificationManager?.createNotificationChannel(notificationChannel)
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.item1 -> {
+                Toast.makeText(this, "Item 1", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.item2 -> {
+                Toast.makeText(this, "Item 2", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.sub_item1 -> {
+                Toast.makeText(this, "Sub item 1", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.sub_item2 -> {
+                Toast.makeText(this, "Sub item 2", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
